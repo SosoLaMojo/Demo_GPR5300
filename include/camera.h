f879adb2f70a65c70fcb26cc540f72625553174f
Solange <sol.sch1990@gmail.com>
2021-06-28 13:59:46 +0200

    Clean code, creating camera.cpp to separate definitions from declarations

--- include/camera.h
+++ include/camera.h
@@ -4,8 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace gl {
-
-	// TODO create camera.cpp for functions definitions
+	
 	// Defines several possible options for camera movement. Used as
 	// abstraction to stay away from window-system specific input methods
 	enum class CameraMovementEnum {
